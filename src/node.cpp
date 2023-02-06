ec574286a61b281ab117321eeca34230d2ebf68d
salihmarangoz <salih285@gmail.com>
2023-02-06 06:19:59 +0100

    deleted

--- src/node.cpp
+++
@@ -1,8 +0,0 @@
-#include <salih_marangoz_thesis/ceres_ik.h>
-
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "salih_marangoz_thesis");
-  salih_marangoz_thesis::CeresIK ik();
-  return 0;
-}
