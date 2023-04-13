7c4ef647fc080ac2cc93551359608b083ad1e0a6
salihmarangoz <salih285@gmail.com>
2023-04-13 18:51:14 +0200

    testing autogenerated configuration

--- include/salih_marangoz_thesis/robot_configuration/robot_configuration.h
+++ include/salih_marangoz_thesis/robot_configuration/robot_configuration.h
@@ -1,8 +1,8 @@
 #ifndef __ROBOT_CONFIGURATION_MAIN_HEADER__
 #define __ROBOT_CONFIGURATION_MAIN_HEADER__
 
-// SET CURRENT ROBOT CONFIGURATION:
-#include <salih_marangoz_thesis/robot_configuration/robot_conf_single_xarm7.h>
+// TODO: this section needs to be dynamically configured while compiling to support multiple robots
 
+#include <salih_marangoz_thesis/robot_configuration/autogen_test.h>
 
 #endif // __ROBOT_CONFIGURATION_MAIN_HEADER__
